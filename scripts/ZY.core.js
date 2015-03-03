f7ea267600414245b989ffd8df8c18bd80e9aaec
Angelia <angeliaw@yeah.net>
2015-03-03 17:33:10 +0800

    add some methods in my library

--- scripts/ZY.core.js
+++ scripts/ZY.core.js
@@ -16,6 +16,72 @@
  			return doc.getElementById(id);
  		},
 
+ 		/**
+ 		 * 去掉字符串头尾空格
+ 		 * @param  {String} text 字符串
+ 		 * @return {String}
+ 		 */
+ 		trim: function(text) {
+ 			return text === null ? '' : text.replace(/(^\s*|\s*$)/g, '');
+ 		},
+
+ 		/**
+ 		 * 转换字符串为对象
+ 		 * @param  {[type]} data [description]
+ 		 * @return {[type]}      [description]
+ 		 */
+ 		parseJSON: function(data) {
+ 			if(!data || typeof data !== 'string') {
+ 				return null;
+ 			}
+ 			data = this.trim(data);
+ 			if (win.JSON && win.JSON.parse) {
+ 				return win.JSON.parse(data);
+ 			} 
+ 			return (new Function('return ' + data ))();
+ 		},
+
+ 		/**
+ 		 * 对象转换为字符串
+ 		 * @param  {Object} obj 对象
+ 		 * @return {String}
+ 		 */
+ 		obj2str: function(obj) {
+
+ 			var str = [];
+ 			var type = typeof obj;
+
+ 			switch (type) {
+ 				case 'undefined':
+ 					str = '';
+ 					break;
+ 				case 'string':
+ 					str = '\"' + obj.replace(/([\"\\])/g, '\\$1').replace(/(\n)/g, '\\n').replace(/(\r)/g, '\\r').replace(/(\t)/g, '\\t') + '\"';
+ 					break;
+ 				case 'object':
+ 					if (!this.isArray(obj)) {
+			            for (var i in obj) {
+			              str.push('\"' + i + '\":' + this.obj2str(obj[i]));
+			            }
+			            if (!!doc.all && !/^\n?function\s*toString\(\)\s*\{\n?\s*\[native code\]\n?\s*\}\n?\s*$/.test(obj.toString)) {
+			              str.push('toString:' + obj.toString.toString());
+			            }
+			            str = '{' + str.join() + '}';
+			        } else {
+			            for (var j = 0; j < obj.length; j++) {
+			              str.push(this.obj2str(obj[j]));
+			            }
+			            str = '[' + str.join() + ']';
+			        }
+ 					break;
+ 				default:
+ 					str = obj.toString().replace(/\"\:/g, '":""');
+ 					break;
+ 			}
+
+ 			return str;
+
+ 		},
 
 
  		/**
@@ -23,24 +89,31 @@
  		 * @param {Object} options 异步请求参数
  		 * 如果type是jsonp
  		 * 如果type是别的类型
- 		 * @return {[type]}
  		 */
  		ajax: function (options) {
 
  			var dataType = options['dataType'].toLocaleLowerCase();
+ 			var data = options.data;
+ 			data = decodeURIComponent(typeof data === 'undefined' ? '' : (typeof data === 'object' ?  ZY.obj2str(data)  : data));
+
 
  			if(dataType === 'jsonp') {
 
  			} else {
 
- 				var xhr = window.ActiveXObject ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
- 				var type = typeof options.type === 'undefined' ? 'get' : options.type;
+ 				var xhr = !window.ActiveXObject ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+ 				var type = typeof options.type === 'undefined' ? 'get' : options.type.toLocaleLowerCase();
+ 				if (type === 'post') {
+ 					xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded;charset=UTF-8');
+ 				} else if(data !== '') {
+ 					options.url = options.url + data;
+ 				}
  				xhr.open(type, options.url, true);
- 				xhr.send(data);
+ 				xhr.send(decodeURIComponent(options.data));
  				xhr.onreadystatechange = function() {
  					if(xhr.readyState === 4) {
- 						if(xhr.state === 200) {
- 							options.successCallback;
+ 						if(xhr.status === 200) {
+ 							options.successCallback(ZY.parseJSON(xhr.responseText));
  						} else {
  							options.failCallback;
  						}
